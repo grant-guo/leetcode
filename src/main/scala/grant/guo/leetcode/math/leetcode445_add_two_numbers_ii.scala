97f025d1fa3fd26db12743cd8043b2971e134b96
grant-guo <garfieldtiger57@gmail.com>
2021-12-09 00:30:13 -0800

    add two numbers ii

--- src/main/scala/grant/guo/leetcode/math/leetcode445_add_two_numbers_ii.scala
+++ src/main/scala/grant/guo/leetcode/math/leetcode445_add_two_numbers_ii.scala
@@ -1,28 +1,48 @@
 package grant.guo.leetcode.math
 
+import grant.guo.leetcode.math.leetcode2_add_two_numbers.{ListNode, addTwoNumbers}
+
 /*
-You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order, and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
+You are given two non-empty linked lists representing two non-negative integers. The most significant digit comes first and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
 
 You may assume the two numbers do not contain any leading zero, except the number 0 itself.
 
-Input: l1 = [2,4,3], l2 = [5,6,4]
-Output: [7,0,8]
-Explanation: 342 + 465 = 807.
+Input: l1 = [7,2,4,3], l2 = [5,6,4]
+Output: [7,8,0,7]
 
 Example 2:
-Input: l1 = [0], l2 = [0]
-Output: [0]
+Input: l1 = [2,4,3], l2 = [5,6,4]
+Output: [8,0,7]
 
 Example 3:
-Input: l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
-Output: [8,9,9,9,0,0,0,1]
-
-
+Input: l1 = [0], l2 = [0]
+Output: [0]
  */
 
-object leetcode2_add_two_numbers extends App {
+
+object leetcode445_add_two_numbers_ii extends App {
+
+  type HEAD=ListNode
+  type TAIL=ListNode
+  def reverseListNode(l: ListNode): (HEAD, TAIL) = {
+    Option(l.next) match {
+      case None => {
+        val node = new ListNode(l.x, null)
+        (node, node)
+      }
+      case Some(v) => {
+        val (head, tail) = reverseListNode(l.next)
+        tail.next = new ListNode(l.x, null)
+        (head, tail.next)
+      }
+    }
+  }
+
 
   def addTwoNumbers(l1: ListNode, l2: ListNode): ListNode = {
+    val reversedL1 = reverseListNode(l1)._1
+    val reversedL2 = reverseListNode(l2)._1
+
     def addTwoNumbersR(l1: ListNode, l2: ListNode, advance: Int): ListNode = {
       (Option(l1), Option(l2)) match {
         case (None, None) => {
@@ -52,26 +72,25 @@ object leetcode2_add_two_numbers extends App {
       }
 
     }
-
-    addTwoNumbersR(l1, l2, 0);
+    val reversedRet = addTwoNumbersR(reversedL1, reversedL2, 0)
+    reverseListNode(reversedRet)._1
   }
 
-   class ListNode(_x: Int = 0, _next: ListNode = null) {
-       var next: ListNode = _next
-       var x: Int = _x
-      def print(): List[Int] = {
-        Option(_next) match {
-          case None => List(_x)
-          case Some(next) => _x :: _next.print()
-        }
-      }
-   }
-
   val ret = addTwoNumbers(
-    new ListNode(2, new ListNode(4, new ListNode(3, null))),
+    new ListNode(7, new ListNode(2, new ListNode(4, new ListNode(3, null)))),
     new ListNode(5, new ListNode(6, new ListNode(4, null)))
   )
 
   println(ret.print())
 
+  class ListNode(_x: Int = 0, _next: ListNode = null) {
+    var next: ListNode = _next
+    var x: Int = _x
+    def print(): List[Int] = {
+      Option(next) match {
+        case None => List(x)
+        case Some(n) => _x :: n.print()
+      }
+    }
+  }
 }
